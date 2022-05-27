356cac08b88e2f1a6c3c7154f58bc28593c50368
Guillermo <Galarzaguillermo.ggm@gmail.com>
2022-05-26 21:50:21 -0300

    files created

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import '../styles/App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <section className='to-do__container'>
+        <h1 className='to-do__tittle'>To Do List</h1>
+
+      </section>
+    </div>
+  )
+}
+
+export default App
