ccf93b28f91807761e9fb436a5df9c115955d76b
Guillermo <Galarzaguillermo.ggm@gmail.com>
2022-05-27 19:08:45 -0300

    some change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,35 @@
-import { useState } from 'react'
 import '../styles/App.css'
 
 function App() {
 
   return (
-    <div className="App">
-      <section className='to-do__container'>
-        <h1 className='to-do__tittle'>To Do List</h1>
+    <div className='App'>
+      <header className='header__container'>
+        <h1 className='header__tittle'>TODO</h1>
+        <button
+          className='header__change-theme'
+        >
+          <img
+            src='src\images\icon-sun.svg'
+            alt='sun icon'
+          />
+        </button>
+      </header>
 
-      </section>
+      <body className='todo__container'>
+        <section className='todo__input'>
+          <label 
+            htmlFor="newTask" 
+            className="todo__input-label"
+          >uwu</label>
+          <input type="text" id="newTask" />
+        </section>
+
+        <section className="todo__list"></section>
+        <section className="todo__panel"></section>
+      </body>
+
+
     </div>
   )
 }
